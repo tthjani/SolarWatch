50f7dbb48a0dc54c66d6e8ff73f1769786bcc5b6
tthjani <tothje98@gmail.com>
2023-11-29 16:35:49 +0100

    feat: added "SolarWatch" class model

---
+++ SolarWatch/SolarWatch.cs
@@ -0,0 +1,9 @@
+﻿namespace SolarWatch;
+
+public class SolarWatch
+{
+    public string Date { get; set; }
+    public string City { get; set; }
+    public string Sunrise { get; set; }
+    public string Sunset { get; set; }
+}
