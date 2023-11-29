995cda7dce259ace8baabaf7f297601ef0f645a8
tthjani <tothje98@gmail.com>
2023-11-29 16:35:27 +0100

    feat: added singletons to "Program.cs"

--- SolarWatch/Program.cs
+++ SolarWatch/Program.cs
@@ -1,3 +1,5 @@
+using SolarWatch.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<ISunriseSunsetDataProvider, SunsetSunriseSunsetApi>();
+builder.Services.AddSingleton<IJsonProcessor, JsonProcessor>();
 
 var app = builder.Build();
 
@@ -21,5 +25,7 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.UseDeveloperExceptionPage();
 
 app.Run();
