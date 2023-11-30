f74ad95dd9ef02e81db145ee9f96e12d3577b709
tthjani <tothje98@gmail.com>
2023-11-30 13:24:15 +0100

    feat: added "GeoJsonProcessor" class to process incoming data from the api

---
+++ SolarWatch/Services/GeoJsonProcessor.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.Json;
+
+namespace SolarWatch.Services;
+
+public class GeoJsonProcessor : IGeoJsonProcessor
+{
+    public List<City> Process(string data)
+    {
+        List<City> cities = new List<City>();
+
+        JsonDocument json = JsonDocument.Parse(data);
+        JsonElement root = json.RootElement;
+
+        if (root.ValueKind == JsonValueKind.Array)
+        {
+            foreach (JsonElement cityElement in root.EnumerateArray())
+            {
+                JsonElement name = cityElement.GetProperty("name");
+                JsonElement lon = cityElement.GetProperty("lon");
+                JsonElement lat = cityElement.GetProperty("lat");
+
+                City cityData = new City
+                {
+                    Name = name.GetString(),
+                    Longitude = lon.GetDouble().ToString(), 
+                    Latitude = lat.GetDouble().ToString()
+                };
+
+                cities.Add(cityData);
+            }
+        }
+
+        return cities;
+    }
+}
