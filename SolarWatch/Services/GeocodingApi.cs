80240a28a2cb00eb3b9eb0a53a139ac12f251b28
tthjani <tothje98@gmail.com>
2024-01-02 10:59:39 +0100

    refact: refactored "GeocodingApi.cs" from sync to async

--- SolarWatch/Services/GeocodingApi.cs
+++ SolarWatch/Services/GeocodingApi.cs
@@ -12,16 +12,17 @@ public class GeocodingApi : ICityNameDataProvider
     }
     
     
-    public string GetCityCoordinates(string name)
+    public async Task<string> GetCityCoordinates(string name)
     {
 
         var apiKey = "ac6d81bdf4f56a57f233fb602e5b9aff";
         var url = $"http://api.openweathermap.org/geo/1.0/direct?q={name}&limit=1&appid={apiKey}";
 
-        var client = new WebClient();
-        
+        using var client = new HttpClient();
         _logger.LogInformation("Calling Geocoding API with url: {url}", url);
-        return client.DownloadString(url);
+
+        var response = await client.GetAsync(url);
+        return await response.Content.ReadAsStringAsync();
     }
     
 }
