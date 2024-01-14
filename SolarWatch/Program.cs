34b68ffcb23d5e0742c77610060d392343bd871f
tthjani <tothje98@gmail.com>
2024-01-14 14:15:18 +0100

    refact: added registration for the new repository classes

--- SolarWatch/Program.cs
+++ SolarWatch/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using SolarWatch.Data;
+using SolarWatch.Repository;
 using SolarWatch.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,12 +9,16 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddDbContext<SolarWatchContext>(options =>
+    options.UseSqlServer("Server=localhost,1435;Database=focused_beaver;User Id=sa;Password=yourStrong(!)Password;Encrypt=false;"));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddSingleton<ISunriseSunsetDataProvider, SunriseSunsetApi>();
-builder.Services.AddSingleton<IJsonProcessor, JsonProcessor>();
-builder.Services.AddSingleton<ICityNameDataProvider, GeocodingApi>();
-builder.Services.AddSingleton<IGeoJsonProcessor, GeoJsonProcessor>();
+builder.Services.AddScoped<ISunriseSunsetDataProvider, SunriseSunsetApi>();
+builder.Services.AddScoped<IJsonProcessor, JsonProcessor>();
+builder.Services.AddScoped<ICityNameDataProvider, GeocodingApi>();
+builder.Services.AddScoped<IGeoJsonProcessor, GeoJsonProcessor>();
+builder.Services.AddScoped<ICityRepository, CityRepository>();
+builder.Services.AddScoped<ISunriseSunsetRepository, SunriseSunsetRepository>();
 
 var app = builder.Build();
 
