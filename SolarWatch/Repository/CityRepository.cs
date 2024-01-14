cba5d0d9b2ebf3f5d565f03f4b77eec46c26d6ed
tthjani <tothje98@gmail.com>
2024-01-14 14:11:21 +0100

    feat: added CityRepository.cs to the project to handle Cities table on DB

---
+++ SolarWatch/Repository/CityRepository.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SolarWatch.Data;
+
+namespace SolarWatch.Repository;
+
+public class CityRepository : ICityRepository
+{
+    private readonly SolarWatchContext _context;
+
+    public CityRepository(SolarWatchContext context)
+    {
+        _context = context;
+    }
+
+
+    public async Task<City?> FindCityByName(string cityName)
+    {
+        var res = await _context.Cities.FirstOrDefaultAsync(c => c.Name == cityName);
+        
+        return res;
+    }
+
+    public async void AddCity(City city)
+    {
+        await _context.AddAsync(city);
+        await _context.SaveChangesAsync();
+    }
+}
