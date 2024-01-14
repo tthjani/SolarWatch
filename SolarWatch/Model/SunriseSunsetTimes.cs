d086f53875f45e428c804459b8253b3e0a771275
tthjani <tothje98@gmail.com>
2024-01-14 14:17:42 +0100

    feat: added SunriseSunsetTimes.cs model class to the project

---
+++ SolarWatch/Model/SunriseSunsetTimes.cs
@@ -0,0 +1,9 @@
+﻿namespace SolarWatch;
+
+public class SunriseSunsetTimes
+{
+    public int Id { get; set; }
+    public DateTimeOffset SunriseDate { get; set; }
+    public DateTimeOffset SunsetDate { get; set; }
+    public int CityId { get; set; }
+}
