79b71ea5e9c64e87d5f8c591a488703256e36600
tthjani <tothje98@gmail.com>
2024-01-14 14:17:25 +0100

    feat: added SunriseSunsetRepository.cs to the project for the DB

---
+++ SolarWatch/Repository/SunriseSunsetRepository.cs
@@ -0,0 +1,19 @@
+﻿using SolarWatch.Data;
+
+namespace SolarWatch.Repository;
+
+public class SunriseSunsetRepository : ISunriseSunsetRepository
+{
+    private readonly SolarWatchContext _context;
+
+    public SunriseSunsetRepository(SolarWatchContext context)
+    {
+        _context = context;
+    }
+    
+    public async Task AddSunriseSunsetTime(SunriseSunsetTimes time)
+    {
+       await _context.AddAsync(time);
+       await _context.SaveChangesAsync();
+    }
+}
