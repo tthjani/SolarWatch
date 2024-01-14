e2338e8776fff3140fd0d5d843e7628896939102
tthjani <tothje98@gmail.com>
2024-01-14 14:16:25 +0100

    feat: added SolarWatchContext.cs class to configure DB

---
+++ SolarWatch/Data/SolarWatchContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SolarWatch.Data;
+
+public class SolarWatchContext : DbContext
+{
+    public DbSet<City> Cities { get; set; }
+    public DbSet<SunriseSunsetTimes> SunriseSunsetTimes { get; set; }
+    
+    public SolarWatchContext(DbContextOptions<SolarWatchContext> options) : base(options)
+    {
+    }
+    
+    /*protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(
+            "Server=localhost,1435;Database=focused_beaver;User Id=sa;Password=yourStrong(!)Password;Encrypt=false;");
+    }*/
+    
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.Entity<City>()
+            .HasIndex(u => u.Name)
+            .IsUnique();
+        
+        builder.Entity<City>()
+            .HasIndex(u => u.Id)
+            .IsUnique();
+
+        builder.Entity<SunriseSunsetTimes>()
+            .HasIndex(u => u.Id)
+            .IsUnique();
+    }
+}
