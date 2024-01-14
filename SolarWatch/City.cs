c5145b94c5db4730513dcb0d59395f0ce5185196
tthjani <tothje98@gmail.com>
2024-01-14 14:10:37 +0100

    feat: City.cs moved to the Model dictionary, added more properties

--- SolarWatch/City.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SolarWatch;
-
-public class City
-{
-    public string Name { get; set; }
-    public string Longitude { get; set; }
-    public string Latitude { get; set; }
-}
---
+++ SolarWatch/Model/City.cs
@@ -0,0 +1,13 @@
+﻿namespace SolarWatch;
+
+public class City
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Longitude { get; set; }
+    public string Latitude { get; set; }
+    //public string State { get; set; }
+    public string Country { get; set; }
+    //public int SunriseSunsetId { get; set; }
+    //public SunriseSunsetTimes SunriseSunsetTimes { get; set; }
+}
